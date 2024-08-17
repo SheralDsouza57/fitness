a39303ed4b109e112b48bd6c483279c747411262
Sheral Dsouza <sheraldsouza57@gmail.com>
2024-08-17 13:29:39 +0530

    Added Images, Icons, Fonts and Colors with their respective constant files.

---
+++ lib/resources/app_icons.dart
@@ -0,0 +1,4 @@
+class AppIcons {
+  static const _prefix = "assets/icons/";
+  static const account = "${_prefix}account.png";
+}
---
+++ lib/resources/app_images.dart
@@ -0,0 +1,4 @@
+class AppImages {
+  static const _prefix = "assets/images/";
+  static const banner1 = "${_prefix}banner1.png";
+}
